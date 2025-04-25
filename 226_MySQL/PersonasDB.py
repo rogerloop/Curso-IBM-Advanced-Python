597238ebf1d84ab91cc148745c274094245803f5
GitHub Desktop RGR <bascular-rendiment0x@icloud.com>
2025-04-25 14:19:22 +0200

    Update .gitignore and refactor PersonasDB.py to use environment variables for database connection

--- 226_MySQL/PersonasDB.py
+++ 226_MySQL/PersonasDB.py
@@ -1,5 +1,24 @@
+# import para variables de entorno seguras y protegidas
+from dotenv import load_dotenv
+import os
+
 # Conectar a la Base de Datos
 import mysql.connector
 
-personas_db = mysql.connector.connect()
+load_dotenv()  # Carga las variables de entorno del archivo .env
+
+personas_db = mysql.connector.connect(
+    host=os.getenv('DB_HOST'),
+    port=int(os.getenv('DB_PORT')),
+    user=os.getenv('DB_USER'),
+    password=os.getenv('DB_PASSWORD'),
+    database=os.getenv('DB_NAME')
+)
+
+# ejecutar la sentencia select
+cursor = personas_db.cursor() # Creo objeto cursor
+cursor.execute('SELECT * FROM personas');
+resultado = cursor.fetchall()
+for persona in resultado:
+    print(persona)
 
