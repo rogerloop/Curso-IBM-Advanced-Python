429fc9a2f8e0d6789c1471f91a5321ed2ce33ff6
GitHub Desktop RGR <bascular-rendiment0x@icloud.com>
2025-05-05 12:11:19 +0200

    Add CSRF token to form in index.html and configure secret key in app.py

--- 292_manejo_flask/app.py
+++ 292_manejo_flask/app.py
@@ -2,8 +2,14 @@ from flask import Flask, render_template
 from cliente_dao import ClienteDAO
 from cliente import Cliente  # Importing the Cliente class
 from cliente_forma import ClienteForm
+import os   # Para mantener llaves secretas
+from dotenv import load_dotenv
+
+load_dotenv()  # Carga las variables de entorno del archivo .env
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 titulo_app ='Zona Fit (GYM)'
 
@@ -17,8 +23,8 @@ def inicio():
     # Creamos un objeto de cliente form vacio
     cliente = Cliente()
     cliente_form = ClienteForm(obj=cliente)
-
-    return render_template('index.html', titulo=titulo_app, clientes=clientes_db, form=cliente_form)
+    return render_template('index.html', titulo=titulo_app, clientes=clientes_db, 
+                           form=cliente_form)
 
 if __name__ == '__main__':
     app.run(debug=True)
